ab43eac443110b3de701bdb7df2cde9d8edc599b
Camille Déglise <camlecomte@etmlnet.local>
2023-11-29 11:26:23 +0100

    modifcation du script en cours

--- Base_Donnees/db_passion.sql
+++ Base_Donnees/db_passion.sql
@@ -1,7 +1,7 @@
 /*
 * Author : Camille Déglise
 * Date : 29.11.2023
-* Modification : --
+* Modification : 29.11.2023 - Insert values 
 * Description : File containt the script MySql for creating the data base of the application 
 */
 DROP DATABASE IF EXISTS db_passion;
@@ -66,3 +66,22 @@ CREATE TABLE t_write(
    FOREIGN KEY(idBook) REFERENCES t_book(idBook),
    FOREIGN KEY(idAuthor) REFERENCES t_author(idAuthor)
 );
+
+-- MODIFICATION NECESSAIRE POUR L ID CATERGORY ET LE pictureCover PATH  -- donc créer l'INSERT pour les catégories également ! 
+INSERT INTO t_book (title, nbPage, bookExtract, resume, editor, yearEdit)
+VALUES 
+  ('Les Hauts de Hurlevent', 342, 'https://www.example.com/excerpt1', 'Une histoire passionnante d''amour et de vengeance sur les landes venteuses d''Angleterre.', 'Éditions Brontë', 1847),
+  ('1984', 298, 'https://www.example.com/excerpt2', 'Dans un monde dystopique, un homme lutte contre un gouvernement totalitaire obsédé par le contrôle.', 'Big Brother Books', 1949),
+  ('Le Seigneur des Anneaux', 1024, 'https://www.example.com/excerpt3', 'Une épopée fantastique de hobbits, d''anneaux magiques et de batailles épiques pour sauver la Terre du Milieu.', 'Middle-earth Press', 1954),
+  ('Orgueil et Préjugés', 426, 'https://www.example.com/excerpt4', 'Une comédie romantique classique explorant les relations au sein de la haute société anglaise du XIXe siècle.', 'Austen Publications', 1813),
+  ('Cent ans de solitude', 368, 'https://www.example.com/excerpt5', 'L''histoire magique et mystique de la famille Buendía dans la ville fictive de Macondo.', 'Solitude Books', 1967),
+  ('Harry Potter à l''école des sorciers', 332, 'https://www.example.com/excerpt6', 'Les aventures de Harry Potter, un jeune sorcier, à l''école de sorcellerie de Poudlard.', 'Sorcerer''s Stone Press', 1997),
+  ('Le Petit Prince', 96, 'https://www.example.com/excerpt7', 'Les réflexions poétiques d''un petit prince voyageant à travers les planètes.', 'Antoine de Saint-Exupéry Éditions', 1943),
+  ('Crime et Châtiment', 576, 'https://www.example.com/excerpt8', 'L''exploration psychologique d''un crime commis par un étudiant tourmenté à Saint-Pétersbourg.', 'Dostoïevski Publishing', 1866),
+  ('Chroniques martiennes', 288, 'https://www.example.com/excerpt9', 'Une série de récits explorant la colonisation de Mars et ses implications sur la condition humaine.', 'Ray Bradbury Press', 1950),
+  ('Fahrenheit 451', 208, 'https://www.example.com/excerpt10', 'Dans une société future, les livres sont interdits, et les pompiers brûlent toute trace de littérature.', 'Bradbury Books', 1953);
+
+INSERT INTO t_user(`login`, firstName, lastName, mailAddress, birthDate, `passWord`, isAdmin, dateInscription)
+VALUES 
+('johndoe', 'John', 'Doe', 'johndoe@example.com', '20-04-1998', 'toto', 0, '29-11-2024'),
+('brondonadams', 'Brondon', 'Adams', 'brondonadams@example.com', '18-02-2003', 1, '29-11-2023');
