5fd50e727d6a2a0c4d0d0e143952775a7ac2a603
Guo Yu <129724299+AlterSpectre@users.noreply.github.com>
2023-12-05 10:08:39 +0100

    branch Guo Yu
    2e push de Guo Yu
    J'espère que cela n'engendre pas plus de problème que le 1er dossier test que j'ai créé ...

---
+++ Laravel/resources/views/index.php
@@ -0,0 +1,79 @@
+ <!--
+    ETML
+    Auteur        :	Déglise Camille - Vougaz Maryline - Wu Guo Yu
+    Date          :	28.11.2023
+    Description   :	Accueil du site "Passion Lecture"
+-->
+
+<?php
+// connexion à la BD
+//include("./DATABASE.php");
+
+//$db = new Database();
+//$teachers = $db->getAllTeachers();
+
+//session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/app.css" rel="stylesheet" media="screen"/>
+    <title>Passion Lecture</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="container-header">
+            <div class="titre-header">
+                <h1>Passion Lecture</h1>
+            </div>
+            
+            <!-- connexion -->
+            <div class="box-login">
+                <label for="user"></label>
+                <input type="text" name="user" id="user" placeholder="Username">
+                <label for="password"></label>
+                <input type="password" name="password" id="password" placeholder="Password">
+                <button type="submit" class="btn btn-login">Login</button>
+            </div>
+            
+
+        </div>
+        <nav>
+            <div class="nav-links">
+                <a href="#">Accueil</a>
+                <a href="#">Liste</a>
+                <a href="#">Ajout</a>
+                <a href="#">Profil</a>
+                <a href="#">Contacts</a>
+            </div>
+            
+        </nav>
+    </header>
+
+    <div class="container">
+        <div class="bloc01">
+            <h3>Nouveautés</h3>
+            <a href="#"><img src="../images/books/livre01.jpg" alt="image01"></a>
+            <a href="#"><img src="../images/books/livre02.jpg" alt="image02"></a>
+            <a href="#"><img src="../images/books/livre03.jpg" alt="image03"></a>
+        </div>
+        <div class="bloc02">
+            <p>texte</p>
+        </div>
+        <!-- <script src="./js/script.js"></script> -->
+    </div>
+
+    <footer>
+        <p>© Camille Déglise - Guo Yu Wu - Maryline Vougaz - 2024</p>
+    </footer>
+
+</body>
+
+</html>
