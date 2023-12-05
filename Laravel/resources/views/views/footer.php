52dff8427a569c52b3c986ffd88c270d4c7b0867
Cammy-Pocket <cdeglise90@gmail.com>
2023-12-05 11:04:38 +0100

    création fichiers pr footer.php & header.php, modification du footer.php

---
+++ Laravel/resources/views/views/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/app.css" rel="stylesheet" media="screen"/>
+    <title>Footer</title>
+</head>
+
+<footer>
+        <p>© <a href="camille.deglise@eduvaud.ch">Camille Déglise</a> - <a href="guoyu.wu@eduvaud.ch">Guo Yu Wu</a> - <a href="maryline.vougaz@eduvaud.ch">Maryline Vougaz</a> - 2024</p>
+</footer>
---
+++ Laravel/resources/views/views/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+    
+</body>
+</html>
+<header>
+        <div class="container-header">
+            <div class="titre-header">
+                <h1>Passion Lecture</h1>
+            </div>
+            
+            <!-- connexion -->
+            <div class="box-login">
+                <label for="user"></label>
+                <input type="text" name="user" id="user" placeholder="Username">
+                <label for="password"></label>
+                <input type="password" name="password" id="password" placeholder="Password">
+                <button type="submit" class="btn btn-login">Login</button>
+            </div>
+            
+
+        </div>
+        <nav>
+            <div class="nav-links">
+                <a href="#">Accueil</a>
+                <a href="#">Liste</a>
+                <a href="#">Ajout</a>
+                <a href="#">Profil</a>
+                <a href="#">Contacts</a>
+            </div>
+            
+        </nav>
+    </header>
