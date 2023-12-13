7d40fa74b76e5ff5235655b9c8e8b40ebb4c0a91
Camille Déglise <camlecomte@etmlnet.local>
2023-12-13 11:21:25 +0100

    controllerBook est commencé (vérif données finies)

--- MVC_Files/Controller/ControllerBook.php
+++ MVC_Files/Controller/ControllerBook.php
@@ -1,22 +1,80 @@
-include ('../Model/ModelBook.php')
+<?php
+include ('../Model/ModelBook.php');
 
-getBooks
-(
-    instancier la classe ModelBook
-    $variable ->getallbooks
-)
+Class ControllerBook {
 
-addBook {
-    
-    // include view AddBook 
+    private function checkBook($datas)
+    {
+        //Validation des données
+        //Vérification du titre 
+        if(!isset($datas["title"]) && ctype_alnum($datas["title"]))
+        {
+            echo "Le titre doit être rempli et il doit être en caractère alpha-numérique";
+        }
+
+        //Vérification du nom de l'auteur
+        if(!isset($datas["nameAuthor"]) && ctype_alpha($datas["nameAuthor"]))
+        {
+            echo "Le nom de l'auteur doit être saisi et il doit êre en caractères alphabétiques exclusivement";
+
+        }
+
+        //Vérification du prénom de l'auteur
+        if(!isset($datas["firstNameAuthor"]) && ctype_alpha($datas["firstNameAuthor"]))
+        {
+            echo "Le prénom de l'auteur doit être saisi et il doit êre en caractères alphabétiques exclusivement";
+
+        }
+
+        //Vérification de catégorie
+        if(!isset($datas["category"]) && ($datas["category"] === ''))
+        {
+            echo "La catégorie doit être choisie";
+        }
+
+        //Vérification du nombre de pages
+        if(!isset($datas["nbPage"]) && ($datas["nbPage"] < 0))
+        {
+            echo "Le nombre de pages doit être saisi et doit être supérieur à 0";
+        }
+
+        //Vérification de l'éditeur 
+        if(!isset($datas["editor"]) && ctype_alnum($datas["editor"]))
+        {
+            echo "Le nom de l'éditeur doit être rempli et doit être en caractère alpha-numérique";
+        }
+
+        //Vérification de l'année d'édition
+        if(!isset($datas["yearEdit"]) && (!preg_match('/^([0-9]{1,4})$/', $datas["yearEdit"])))
+        {
+            echo "L'année d'édition doit être remplie et ne doit que comporter 4 chiffres, comme 1990";
+        }
+
+        //Vérification du lien pour l'extrait 
+         if(!isset($datas["bookExtract"]) && ($datas["bookExtract"] === ''))
+         {
+             echo "L'URL pour un extrait doit être saisi";
+         }
+
+         //Vérification du résumé
+        if(!isset($datas["resume"]) && ($datas["resume"] === ''))
+        {
+            echo "Un résumé doit être rempli";
+        }
+
+
+        // Si données pas ok 
+        // retour sur le formulaire
+
+    }
+
+    public function addBook($datas) {
+        $this->checkBook($datas);
+
+        // Appel ModelBook pour INSERT
+
+    }
 }
 
-bookDetail {
 
-}
-
-authorsDetal
-{
-    $variable = .....
-}
 
---
+++ MVC_Files/View/resources/PHP_html/dispatch.php
@@ -0,0 +1,43 @@
+<?php 
+
+// dispatch.php?controler=book&action=add
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+
+echo "<pre>";
+var_dump($_GET);
+echo "</pre>";
+
+
+$data = $_POST;
+
+
+$controler = $_GET["controler"];
+$action = $_GET["action"];
+
+if ($controler === "book") {
+    $controlerBook = new ControllerBook();
+
+    if ($action === "add") {
+        $controlerBook->addBook($data);
+    }
+
+    if ($action === "update") {
+        //$controlerBook->updateBook($data);
+    }
+}
+
+if ($controler === "user") {
+
+    // instancier le user
+
+    if ($action === "add") {
+
+        // appeler la bonne méthode du controlleur user
+        $controlerBook->addBook($data);
+    }
+}
+
+
