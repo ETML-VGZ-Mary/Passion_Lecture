2f1c9244a861bab6f45b345efb3fd8c7fc8a675d
cdeglise90@gmail.com <cdeglise90@gmail.com>
2023-12-19 09:27:39 +0100

    Model author fini (normalement)

--- MVC_Files/Model/ModelAuthor.php
+++ MVC_Files/Model/ModelAuthor.php
@@ -39,11 +39,11 @@ Class ModelAuthor extends ModelMain {
     /**
      * Méthode qui récupère la liste des informations pour 1 livre
      * Prend en argument l'ID du livre
-     * Doit être associée à un $_GET dans la page concernée
+     * Doit être associée à un $_POST dans la page concernée
      * Utilisée dans la page "Détail d'un livre"
      */
     public function getOneAuthor($id){
-        // Récupère les données sur la table livre avec une requête sql
+        // Récupère les données sur la table auteur avec une requête sql
         // en utilisant son ID
         $query = "SELECT * FROM t_author WHERE idAuthor= :id"; 
         $binds = [
@@ -54,12 +54,39 @@ Class ModelAuthor extends ModelMain {
         $req = $this->queryPrepareExecute($query,$binds);
 
         //Retourne dans un tableau associatif à une seule 
-        //entrée les données d'un livre
+        //entrée les données d'un auteur
         $author =$this->formatData($req);
         
         //Retourne la première (et unique) entrée du tableau
         return $author[0];
     }
+
+
+    /**
+     * Méthode pour ajouter un auteur dans la base de données
+     * Prend en argument les données entrées par l'utilisateur
+     */
+
+     public function addAuthor($datas)
+     {
+        //Ajout des données de $_POST dans de nouvelles variables
+        $firstName = $datas["firstNameAuthor"];
+        $lastName = $datas["nameAuthor"];
+
+        //Requête sur la db pour ajout avec un prepare
+        $query = "INSERT INTO t_author(firstName, lastName)
+        VALUES (:firstName, :lastName)";
+
+        $binds = [
+            ['firstName', $firstName, PDO::PARAM_STR],
+            ['lastName', $lastName, PDO::PARAM_STR]
+        ];
+        $this->queryPrepareExecute($query, $binds);
+
+     }
+
+     
+    
 
 }
    
